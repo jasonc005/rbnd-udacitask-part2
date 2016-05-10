1f06e4e90515c85fa1938771d6b7116ed8bc1e04
Jason <jason.chandlerp@gmail.com>
2016-05-10 14:20:37 -0400

    Implement IndexExceedsListSize and InvalidPriorityValue errors.

--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -9,7 +9,7 @@ class UdaciList
 
   def add(type, description, options={})
     type = type.downcase
-    if is_valid_type?(type)
+    if is_valid_type?(type) && (options[:priority] ? is_valid_priority?(options[:priority]) : true)
       @items.push TodoItem.new(description, options) if type == "todo"
       @items.push EventItem.new(description, options) if type == "event"
       @items.push LinkItem.new(description, options) if type == "link"
@@ -17,7 +17,7 @@ class UdaciList
   end
 
   def delete(index)
-    @items.delete_at(index - 1)
+    @items.delete_at(index - 1) if is_valid_index? (index)
   end
 
   def all
@@ -30,10 +30,28 @@ class UdaciList
   end
 
   def is_valid_type? (type)
-    if type == "todo" || type == "event" || type == "link"
+    types = ["todo","event","link"]
+    if types.include? type
       true
     else
       raise InvalidItemType, "#{type} is not a valid item type."
+    end
+  end
+
+  def is_valid_index? (index)
+    if index <= @items.length
+      true
+    else
+      raise IndexExceedsListSize, "#{index} is larger than the list size."
+    end
+  end
+
+  def is_valid_priority? (priority)
+    values = ["high","medium","low"]
+    if values.include? priority
+      true
+    else
+      raise InvalidPriorityValue, "#{priority} is an invalid priority value."
     end
   end
 
