d3fc6eafd00d815a512f57da55336d2e3afdc647
Jason <jason.chandlerp@gmail.com>
2016-05-10 13:26:26 -0400

    Add error classes and implement InvalidItemType error.

--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -1,19 +1,25 @@
 class UdaciList
+  include UdaciListErrors
   attr_reader :title, :items
 
   def initialize(options={})
-    @title = options[:title]
+    @title = options[:title] || "Untitled List"
     @items = []
   end
+
   def add(type, description, options={})
     type = type.downcase
-    @items.push TodoItem.new(description, options) if type == "todo"
-    @items.push EventItem.new(description, options) if type == "event"
-    @items.push LinkItem.new(description, options) if type == "link"
+    if is_valid_type?(type)
+      @items.push TodoItem.new(description, options) if type == "todo"
+      @items.push EventItem.new(description, options) if type == "event"
+      @items.push LinkItem.new(description, options) if type == "link"
+    end
   end
+
   def delete(index)
     @items.delete_at(index - 1)
   end
+
   def all
     puts "-" * @title.length
     puts @title
@@ -22,4 +28,13 @@ class UdaciList
       puts "#{position + 1}) #{item.details}"
     end
   end
+
+  def is_valid_type? (type)
+    if type == "todo" || type == "event" || type == "link"
+      true
+    else
+      raise InvalidItemType, "#{type} is not a valid item type."
+    end
+  end
+
 end
