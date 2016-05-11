165aec487dcf102127427e10d0540b4a3d8c62f1
Jason <jason.chandlerp@gmail.com>
2016-05-10 20:57:52 -0400

    Move error checking methods from Udacilist class to UdaciListErrors module.

--- lib/errors.rb
+++ lib/errors.rb
@@ -8,4 +8,31 @@ module UdaciListErrors
 
   class InvalidPriorityValue < StandardError
   end
+
+  def is_valid_type? (type)
+    types = ["todo","event","link"]
+    if types.include? type
+      true
+    else
+      raise InvalidItemType, "#{type} is not a valid item type."
+    end
+  end
+
+  def is_valid_index? (index)
+    if index <= @items.length
+      true
+    else
+      raise IndexExceedsListSize, "#{index} is larger than the list size."
+    end
+  end
+
+  def is_valid_priority? (priority)
+    values = ["high","medium","low"]
+    if values.include? priority
+      true
+    else
+      raise InvalidPriorityValue, "#{priority} is an invalid priority value."
+    end
+  end
+  
 end
--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -29,30 +29,4 @@ class UdaciList
     end
   end
 
-  def is_valid_type? (type)
-    types = ["todo","event","link"]
-    if types.include? type
-      true
-    else
-      raise InvalidItemType, "#{type} is not a valid item type."
-    end
-  end
-
-  def is_valid_index? (index)
-    if index <= @items.length
-      true
-    else
-      raise IndexExceedsListSize, "#{index} is larger than the list size."
-    end
-  end
-
-  def is_valid_priority? (priority)
-    values = ["high","medium","low"]
-    if values.include? priority
-      true
-    else
-      raise InvalidPriorityValue, "#{priority} is an invalid priority value."
-    end
-  end
-
 end
